f99b7a44b10f192c923bf8855807224df1de325f
Megan McNeill <mcneim2@gmail.com>
2024-07-04 22:27:02 -0400

    updated view all employees query to join

--- db/query.sql
+++ db/query.sql
@@ -7,8 +7,10 @@ SELECT department_name AS "Department Name", id AS "Department ID" FROM departme
 SELECT roles.title AS "Title", roles.id AS "Role ID", departments.department_name AS "Department Name", roles.salary AS "Salary" FROM roles INNER JOIN departments ON departments.id = roles.department_id;
 
 -- view all employees
--- formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+-- formatted table showing employee data, including employee ids, first names, last names, , departments, job titles, salaries, and managers that the employees report to
 SELECT id, first_name AS "First Name", last_name AS "Last Name" FROM employees AS "Employees";
+
+SELECT employees.id, employees.first_name, employees.last_name, departments.department_name, roles.title, roles.salary FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON departments.id = roles.department_id;
 
 SELECT * FROM departments;
 SELECT * FROM roles;

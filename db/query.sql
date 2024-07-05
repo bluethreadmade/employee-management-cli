9de3daac6eb3ba18d1c84e95f8fbbee9bbb3b33f
Megan McNeill <mcneim2@gmail.com>
2024-07-04 22:29:46 -0400

    updated view all employees query to include alias

--- db/query.sql
+++ db/query.sql
@@ -8,9 +8,7 @@ SELECT roles.title AS "Title", roles.id AS "Role ID", departments.department_nam
 
 -- view all employees
 -- formatted table showing employee data, including employee ids, first names, last names, , departments, job titles, salaries, and managers that the employees report to
-SELECT id, first_name AS "First Name", last_name AS "Last Name" FROM employees AS "Employees";
-
-SELECT employees.id, employees.first_name, employees.last_name, departments.department_name, roles.title, roles.salary FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON departments.id = roles.department_id;
+SELECT employees.id AS "Employee ID", employees.first_name AS "First Name", employees.last_name AS "Last Name", departments.department_name AS "Department", roles.title AS "Title", roles.salary AS "Salary" FROM employees INNER JOIN roles ON employees.role_id = roles.id INNER JOIN departments ON departments.id = roles.department_id;
 
 SELECT * FROM departments;
 SELECT * FROM roles;
